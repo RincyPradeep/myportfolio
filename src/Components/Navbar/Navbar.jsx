6b8d2ddc5696ce25b761a0cda661aab657291007
Rincy Pradeep <rincypradeep15@gmail.com>
2021-09-16 13:43:39 +0530

    Resume changed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
         <Link to="/projects" className={activeClass === 'projects' ? "menu-item active-item" : "menu-item"} onClick={()=>setActiveClass('projects')}>Projects</Link>
         <Link to="/skills" className={activeClass === 'skills' ? "menu-item active-item" : "menu-item"} onClick={()=>setActiveClass('skills')}>Skills</Link>
         <Link to="/contact" className={activeClass === 'contact' ? "menu-item active-item" : "menu-item"} onClick={()=>setActiveClass('contact')}>Contact</Link>  
-        <a href="https://docs.google.com/document/d/1Z5zvlvJ94czo3L9ESmjaaynMM5pktIZT1NTWfme1Ig0/edit?usp=sharing" target="_blank" rel="noopener noreferrer"
+        <a href="https://docs.google.com/document/d/1PlS1M4_-DXmC7wRR-Az_id4TP7tvMiHm5j4Os3HsUPw/edit?usp=sharing" target="_blank" rel="noopener noreferrer"
                   className={activeClass === 'resume' ? "menu-item active-item" : "menu-item"} onClick={()=>setActiveClass('resume')}>
             Resume</a>     
       </Menu>
